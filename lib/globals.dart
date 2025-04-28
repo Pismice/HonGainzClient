952d0961bd5df208e1109815393f8398dcc44646
Pismice <santoro.jeremie@outlook.com>
2025-04-29 00:19:51 +0200

    https url

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 late final FlutterSecureStorage storage;
-//String baseUrl = 'https://hongainz.onrender.com/';
-String baseUrl = 'http://91.99.14.212:8080/';
+//String baseUrl = 'http://91.99.14.212:8080/';
+//String baseUrl = 'http://pismice.com:8080/';
+String baseUrl = 'https://pismice.com:443/';
