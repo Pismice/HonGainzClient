221ca52ecc739d1463a5c38e45085ef38d3d2ed8
Pismice <santoro.jeremie@outlook.com>
2025-05-18 23:31:55 +0200

    changed variable pour prod

--- lib/globals.dart
+++ lib/globals.dart
@@ -2,7 +2,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter/material.dart';
 
 late final FlutterSecureStorage storage;
-String baseUrl = 'http://localhost:8080/';
-//String baseUrl = 'https://pismice.com:443/';
+//String baseUrl = 'http://localhost:8080/';
+String baseUrl = 'https://pismice.com:443/';
 
 final RouteObserver<ModalRoute> routeObserver = RouteObserver<ModalRoute>();
